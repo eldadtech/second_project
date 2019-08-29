d2657dbc302d202ac56fbb242e0c815c484b8dbd
Eldad <eldadtech@gmail.com>
2019-08-29 12:12:50 +0300

    selenium

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,28 +1,24 @@
 from selenium import webdriver
+import time
+import logging
+LOGGER = logging.getLogger('Error')  # initiate logger
+
+
 def end_automation(driver):
     time.sleep(2)  # just to view the result before closing
     driver.close()
     driver.quit()
 
 
-#from selenium.webdriver.common.keys import Keys
-import time
-import logging
-LOGGER = logging.getLogger('Error')  # initiate logger
-
 # get chrome driver
 driver = webdriver.Chrome(executable_path="C:\\code\\chromedriver_win32\\chromedriver.exe")
-driver.implicitly_wait(20)
+driver.implicitly_wait(10)
 # Enter the Site
 try:
     driver.get("http://192.168.99.101:5000/")
-# time.sleep(10)
-   # hello_text = driver.find_element_by_tag_name('pre')
+# Get the text and print to console
     hello_text = driver.find_element_by_xpath("/html//body")
-
-
-
-    print(hello_text)
+    print(hello_text.text)
 
 except BaseException as e:  # catch error and print it
     LOGGER.error("failed " + str(e))
